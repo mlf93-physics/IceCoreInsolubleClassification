8177df78c5f18a0d3040cd2307f2dd2ee143c9a5
Martin <martin.frolund@gmail.com>
2021-06-07 11:45:05 +0200

    Added probabilities to the random data augmentation transforms

--- martin/utilities/constants.py
+++ martin/utilities/constants.py
@@ -22,19 +22,25 @@ CLASSES = np.array([1, 2, 3, 4, 5, 6])
 SEED = 197
 
 TRAIN_TRANSFORM = tv_transforms.Compose([
-    tv_transforms.RandomCrop(80, pad_if_needed=True),
+    tv_transforms.RandomApply([
+        tv_transforms.RandomCrop(80, pad_if_needed=True)],
+        p=0.10),
     tv_transforms.Resize([IMAGE_WIDTH, IMAGE_HEIGHT],
         interpolation=tv_transforms.InterpolationMode.BILINEAR),
     tv_transforms.Normalize(mean=[60], std=[30], inplace=True),
-    tv_transforms.RandomHorizontalFlip(),
-    tv_transforms.GaussianBlur(5),
+    tv_transforms.RandomHorizontalFlip(p=0.10),
+    tv_transforms.RandomVerticalFlip(p=0.10),
+    tv_transforms.RandomApply([tv_transforms.GaussianBlur(5)], p=0.10)
 ])
 
 VAL_TRANSFORM = tv_transforms.Compose([
-    tv_transforms.RandomCrop(80, pad_if_needed=True),
+    tv_transforms.RandomApply([
+        tv_transforms.RandomCrop(80, pad_if_needed=True)],
+        p=0.10),
     tv_transforms.Resize([IMAGE_WIDTH, IMAGE_HEIGHT],
         interpolation=tv_transforms.InterpolationMode.BILINEAR),
     tv_transforms.Normalize(mean=[60], std=[30], inplace=True),
-    tv_transforms.RandomHorizontalFlip(),
-    tv_transforms.GaussianBlur(5),
+    tv_transforms.RandomHorizontalFlip(p=0.10),
+    tv_transforms.RandomVerticalFlip(p=0.10),
+    tv_transforms.RandomApply([tv_transforms.GaussianBlur(5)], p=0.10)
 ])
