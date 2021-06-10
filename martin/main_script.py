6f03cd7316743d2850fd4f361d5cf04e1ffc25d4
Martin <martin.frolund@gmail.com>
2021-06-10 15:36:10 +0200

    Tried out MultiLabelMarginLoss

--- martin/main_script.py
+++ martin/main_script.py
@@ -36,7 +36,7 @@ def run_torch_CNN(args, train_dataloader=None, val_dataloader=None,
     print('Initialising torch CNN')
     t_cnn = cnns.TorchNeuralNetwork().to(DEVICE)
     print(t_cnn)
-    criterion = t_nn.CrossEntropyLoss()
+    criterion = t_nn.MultiLabelMarginLoss()
     # optimizer = t_optim.SGD(t_cnn.parameters(), lr=0.001, momentum=0.9)
     optimizer = t_optim.Adam(t_cnn.parameters(), lr=0.001)
 
